a7ef532d9eacda1f195dab5f99ac747ec71162d9
Brandon Bloch <brandon@bloch.ca>
2023-05-30 12:40:26 -0400

    Extract machine limits logic to separate util

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { BrimType, GCodeFlavor, InfillPattern, SeamPosition, SolidFillPattern } from './enums';
+import { applyMachineLimits } from './machine-limits';
 import Profile from './profile';
 import { applyStartSequenceDefaults, convertToPrinterScript } from './sequences';
 import type { MachineLimits } from './types/machine-limits';
@@ -738,60 +739,7 @@ const index = ({
 
   // machine limits (if provided)
   if (machineLimits) {
-    if (machineLimits.maxFeedrate) {
-      if (machineLimits.maxFeedrate.x !== undefined) {
-        profile.machineMaxFeedrateX = [machineLimits.maxFeedrate.x, machineLimits.maxFeedrate.x];
-      }
-      if (machineLimits.maxFeedrate.y !== undefined) {
-        profile.machineMaxFeedrateY = [machineLimits.maxFeedrate.y, machineLimits.maxFeedrate.y];
-      }
-      if (machineLimits.maxFeedrate.z !== undefined) {
-        profile.machineMaxFeedrateZ = [machineLimits.maxFeedrate.z, machineLimits.maxFeedrate.z];
-      }
-      if (machineLimits.maxFeedrate.e !== undefined) {
-        profile.machineMaxFeedrateE = [machineLimits.maxFeedrate.e, machineLimits.maxFeedrate.e];
-      }
-    }
-    if (machineLimits.maxAcceleration) {
-      if (machineLimits.maxAcceleration.x !== undefined) {
-        profile.machineMaxAccelerationX = [machineLimits.maxAcceleration.x, machineLimits.maxAcceleration.x];
-      }
-      if (machineLimits.maxAcceleration.y !== undefined) {
-        profile.machineMaxAccelerationY = [machineLimits.maxAcceleration.y, machineLimits.maxAcceleration.y];
-      }
-      if (machineLimits.maxAcceleration.z !== undefined) {
-        profile.machineMaxAccelerationZ = [machineLimits.maxAcceleration.z, machineLimits.maxAcceleration.z];
-      }
-      if (machineLimits.maxAcceleration.e !== undefined) {
-        profile.machineMaxAccelerationE = [machineLimits.maxAcceleration.e, machineLimits.maxAcceleration.e];
-      }
-      if (machineLimits.maxAcceleration.extruding !== undefined) {
-        profile.machineMaxAccelerationExtruding = [
-          machineLimits.maxAcceleration.extruding,
-          machineLimits.maxAcceleration.extruding,
-        ];
-      }
-      if (machineLimits.maxAcceleration.retracting !== undefined) {
-        profile.machineMaxAccelerationRetracting = [
-          machineLimits.maxAcceleration.retracting,
-          machineLimits.maxAcceleration.retracting,
-        ];
-      }
-    }
-    if (machineLimits.maxJerk) {
-      if (machineLimits.maxJerk.x !== undefined) {
-        profile.machineMaxJerkX = [machineLimits.maxJerk.x, machineLimits.maxJerk.x];
-      }
-      if (machineLimits.maxJerk.y !== undefined) {
-        profile.machineMaxJerkY = [machineLimits.maxJerk.y, machineLimits.maxJerk.y];
-      }
-      if (machineLimits.maxJerk.z !== undefined) {
-        profile.machineMaxJerkZ = [machineLimits.maxJerk.z, machineLimits.maxJerk.z];
-      }
-      if (machineLimits.maxJerk.e !== undefined) {
-        profile.machineMaxJerkE = [machineLimits.maxJerk.e, machineLimits.maxJerk.e];
-      }
-    }
+    applyMachineLimits(profile, machineLimits);
   }
 
   return profile;
